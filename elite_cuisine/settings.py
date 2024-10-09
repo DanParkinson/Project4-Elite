47148dce0111491c8be9e98845ca24c0035a4b15
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-09 14:37:04 +0000

    Secret key added

--- elite_cuisine/settings.py
+++ elite_cuisine/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&-+cd&!7e2k5zj^517et2mwoqd*a5z=v*l)jevzr3lz-^ij1zo'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
