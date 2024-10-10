f7eb2d5f1a1bfbcfd95f2508b9ec6d0bb266f50b
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-10 13:07:49 +0000

    registration form phone number added

--- home/forms.py
+++ home/forms.py
@@ -7,8 +7,9 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    phone_number = forms.CharField(max_length=15, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'phone_number' 'password1', 'password2')
 
